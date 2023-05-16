3b297491352e14fd6f18e57cca8d74b6a31a76d9
santiadlv <49079265+santiadlv@users.noreply.github.com>
2023-05-15 22:29:44 -0600

    Develop main interactive scene with first 3D models

--- src/main.js
+++ src/main.js
@@ -1,34 +1,68 @@
 import * as THREE from 'three';
+import { STLLoader } from 'three/addons/loaders/STLLoader';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-// init
+let camera, scene, renderer, controls;
 
-const camera = new THREE.PerspectiveCamera(
-  70,
-  window.innerWidth / window.innerHeight,
-  0.01,
-  10
-);
+init();
+animate();
 
-camera.position.z = 1;
+function init() {
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const scene = new THREE.Scene();
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
 
-const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
+    scene.add(new THREE.AxesHelper(5));
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+    camera = new THREE.PerspectiveCamera(
+        50,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    
+    camera.rotation.set(-Math.PI / 6, -Math.PI / 4, -Math.PI / 9);
+    camera.position.set(-90, 60, 90);
+		scene.add( camera );
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animation);
-document.body.appendChild(renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+		controls.minDistance = 0.5;
+		controls.maxDistance = 1000;
 
-// animation
+    const grid = new THREE.GridHelper(100, 10, new THREE.Color('black'), new THREE.Color('black'));
+    scene.add(grid);
 
-function animation(time) {
-  mesh.rotation.x = time / 2000;
-  mesh.rotation.y = time / 1000;
+    const material = new THREE.MeshNormalMaterial();
 
-  renderer.render(scene, camera);
+    const loader = new STLLoader();
+    loader.load(
+        './models/toy-basket.stl',
+        function (geometry) {
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.scale.set(0.1, 0.1, 0.1);
+            mesh.rotation.set(-Math.PI / 2, 0, 0);
+            scene.add(mesh);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+}
+
+function render() {
+  renderer.render(scene, camera)
 }
